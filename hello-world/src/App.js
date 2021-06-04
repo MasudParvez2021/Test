e2c5b3d6cd0b2e8a66a7bf9291adcc8642c9fb51
MasudParvez2021 <mdmasudparvez786@outlook.com>
2021-06-04 20:49:48 +0530

    This is hello world program

---
+++ hello-world/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Greet from './components/Greet';
+
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+          <Greet />
+      </div>
+    );
+     
+  }
+}
+
+export default App;
